f6de34aaa72d174f968af38958d2ea50144bf3e6
simaran sahoo <simaransahoo40@gmail.com>
2026-06-11 16:45:29 +0530

    backend port added

--- Backend/database.py
+++ Backend/database.py
@@ -11,7 +11,7 @@ from urllib.parse import quote_plus
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = quote_plus(os.getenv('DB_PASSWORD') or "")
 DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+DB_PORT = int(os.getenv("DB_PORT") or "3306")
 DB_NAME = os.getenv('DB_NAME')
 
 DB_URL = (
